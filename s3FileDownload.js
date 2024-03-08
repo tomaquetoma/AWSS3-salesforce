43a10fd771d763b8089ae0c5b73aea9d0df6fc07
Tomas Di Luca Irazusta <94632181+tomaquetoma@users.noreply.github.com>
2024-03-08 08:32:12 -0300

    Add files via upload

--- s3FileDownload.js
+++ s3FileDownload.js
@@ -1,3 +1,93 @@
+import { LightningElement, api, wire } from 'lwc';
+import { loadScript } from 'lightning/platformResourceLoader';
+import AWSSDK from '@salesforce/resourceUrl/AWSSDK';
+import { getRecord } from 'lightning/uiRecordApi';
+import LegacyDocumentName from "@salesforce/schema/Legacy_Document__c.Name";
+import LegacyDocumentRevision from "@salesforce/schema/Legacy_Document__c.Document_Revision__c";
+import { CloseActionScreenEvent } from 'lightning/actions';
+
+const fields = [LegacyDocumentName, LegacyDocumentRevision];
+
+export default class s3FileDownload extends LightningElement {
+    @api recordId;
+    fileName;
+    key;
+    legacyDocument;
+
+    @wire(getRecord, { recordId: '$recordId', fields: fields })
+    wiredRecord(result) {
+        this.legacyDocument = result;
+    }    
+
+    connectedCallback() {
+
+        if (!window.AWS) {
+            // console.log("LOADINGGG AWS SDK");
+            loadScript(this, AWSSDK)
+                .then(() => {
+                    // console.log("AWS SDK loaded");
+                    this.downloadFile();
+                    this.closeQuickAction();                  
+                })
+                .catch(error => {
+                    console.error("Error loading AWS SDK: ", error);
+                });
+        } 
+        else {
+            // console.log("AWS SDK already LOADEDDDDDD");
+            new Promise((resolve, reject) => {
+                const interval = setInterval(() => {
+                    if (this.recordId) {
+                        clearInterval(interval);
+                        resolve();
+                    }
+                }, 1000);
+            })
+            .then(() => {
+                this.downloadFile();
+                this.closeQuickAction();
+            })
+            .catch(error => {
+                console.error("Error loading AWS SDK: ", error);
+            });
+        }
+
+    }
+
+    downloadFile() {
+
+        if (this.legacyDocument.data) {
+            this.fileName = this.legacyDocument.data.fields.Name.value;
+            this.key = this.legacyDocument.data.fields.Document_Revision__c.value;
+
+            const AWS = window.AWS;
+            AWS.config.update({ accessKeyId: "AKIAQDB7BCZELX3JHESU", secretAccessKey: atob("SWJTUXYxbHZMUDIxVEh2THVIVEMxWTd4QnAxa1o3cGFLMlYrbFZxWQ==") });
+            let s3 = new AWS.S3();
+            let params = {
+                Bucket: "s3lyon-data",
+                Key: this.key,
+                Expires: 3600,
+                ResponseContentDisposition: `attachment; filename="${this.fileName}"`
+            };
+            let url = s3.getSignedUrl("getObject", params);
+            //console.log(url);
+            window.open(url);
+        }
+    }
+
+    closeQuickAction() {
+        this.dispatchEvent(new CloseActionScreenEvent());
+    }
+
+}
+
+// https://s3lyon-data.s3.amazonaws.com/000011d0-f260-11ec-80cb-067d538c0c10?
+// AWSAccessKeyId=AKIAQDB7BCZELX3JHESU&
+// Expires=1709648020&
+// Signature=ayb2nebqSrjGfkJQHRPyOmgGINs%3D&
+// response-content-disposition=attachment%3B%20filename%3D%226MB%20file.pdf%22
+
+
 // import { LightningElement,api,wire } from 'lwc';
 // import getFileData from '@salesforce/apex/AWSFileService.getDocumentUsingFileId';
 
@@ -90,71 +180,3 @@
 
 
 
-import { LightningElement, api, wire } from 'lwc';
-import { loadScript } from 'lightning/platformResourceLoader';
-import AWSSDK from '@salesforce/resourceUrl/AWSSDK';
-import { getRecord } from 'lightning/uiRecordApi';
-import LegacyDocumentName from "@salesforce/schema/Legacy_Document__c.Name";
-import LegacyDocumentRevision from "@salesforce/schema/Legacy_Document__c.Document_Revision__c";
-
-const fields = [LegacyDocumentName, LegacyDocumentRevision];
-
-export default class s3FileDownload extends LightningElement {
-    @api recordId;
-    fileName;
-    key;
-    legacyDocument;
-
-    @wire(getRecord, { recordId: '$recordId', fields: fields })
-    wiredRecord(result) {
-        this.legacyDocument = result;
-        if (result.data) {
-            this.refreshData();
-        }
-    }
-
-    connectedCallback() {
-        if (!window.AWS) {
-            console.log("Loading AWS SDK");
-            loadScript(this, AWSSDK)
-                .then(() => {
-                    console.log("AWS SDK loaded");
-                    this.downloadFile();
-                    //window.location.reload()
-                })
-                .catch(error => {
-                    console.error("Error loading AWS SDK: ", error);
-                });
-        } else {
-            console.log("AWS SDK already loaded");
-            this.downloadFile();
-            //window.location.reload()
-        }
-    }
-
-    downloadFile() {
-
-        if (this.legacyDocument.data) {
-            this.fileName = this.legacyDocument.data.fields.Name.value;
-            this.key = this.legacyDocument.data.fields.Document_Revision__c.value;
-
-            console.log('this.fileName ' + this.fileName)
-            console.log('this.key ' + this.key)
-        
-
-            const AWS = window.AWS;
-            AWS.config.update({ accessKeyId: "AKIAQDB7BCZELX3JHESU", secretAccessKey: atob("SWJTUXYxbHZMUDIxVEh2THVIVEMxWTd4QnAxa1o3cGFLMlYrbFZxWQ==") });
-            let s3 = new AWS.S3();
-            //let fileName = "test.pdf";
-            let params = {
-                Bucket: "s3lyon-data",
-                Key: this.key,
-                Expires: 3600,
-                ResponseContentDisposition: `attachment; filename="${this.fileName}"`
-            };
-            let url = s3.getSignedUrl("getObject", params);
-            console.log(url);
-            window.open(url);
-        }
-    }
-}
